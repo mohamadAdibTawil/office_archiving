67966a99d573750416a1b0b65a7649d3f9792864
Mohamad Adib Tawil <https://github.com/mohamadAdibTawil/notes_app.git>
2024-01-08 21:45:43 +0300

    home screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,12 +1,17 @@
 import 'dart:ui';
 
 const officeArchiving = 'Office Archiving';
+
 const kFontGTSectraFine = 'GT Sectra Fine';
+
 const Color kPrimaryColor = Color(0xFF2ACF7F);
+
 const Color kPOpacityColor = Color(0xffAEFFBF);
 
 const Color kWhiteColor = Color(0xffffffff);
+
 const kSectionBox = 'SectionBox';
+
 const kItemSectionBox = 'ItemSectionBox';
 
 const String kOfficePicure = 'assets/images/office_image_one.png';
